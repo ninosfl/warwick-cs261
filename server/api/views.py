2e1a0380ea25bc34d453ca0c9f25b00ce1e9d6b8
Proxymoron461 <tobyjonahbee@gmail.com>
2020-01-28 21:57:06 +0000

    Added url for api_main view (only local to api app).

--- server/api/views.py
+++ server/api/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 # Create your views here.
-def main(request):
+def api_main(request):
     return JsonResponse({"implemented": False})
