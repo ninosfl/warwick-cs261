2065f5750866d7099ef6379e4eddaf6dd47222d2
T Bailey <tobyjonahbee@gmail.com>
2020-02-11 20:55:15 +0000

    Added new url paths and views for listing reports.

--- server/reports/urls.py
+++ server/reports/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
-from .views import yearly_report
+from .views import years, months, days, report
 
+# path("", yearly_report, name="reports-yearly")
 urlpatterns = [
-    path("", yearly_report, name="reports-yearly")
+    path("", years, name="report-list-years"),
+    path("<int:year>/", months, name="report-list-months"),
+    path("<int:year>/<int:month>/", days, name="report-list-days"),
+    path("<int:year>/<int:month>/<int:day>/", report, name="report-get")
 ]
--- server/reports/views.py
+++ server/reports/views.py
@@ -1,25 +1,42 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-def yearly_report(request):
-    if request.method == "POST":
-        mockdata = [
-            {
-                "date": "1 january",
-                "amount": "loads"
-            },
-            {
-                "date": "2 january",
-                "amount": "maybe less"
-            },
-            {
-                "date": "1 april",
-                "amount": "incredible quantity"
-            }
-        ]
-        context = {
-            "method": request.method,
-            "year": request.POST.get("year"),
-            "yearlydata": mockdata
-        }
-        return render(request, "reports/yearly.html", context)
-    return render(request, "reports/yearly.html")
+# def yearly_report(request):
+#     if request.method == "POST":
+#         mockdata = [
+#             {
+#                 "date": "1 january",
+#                 "amount": "loads"
+#             },
+#             {
+#                 "date": "2 january",
+#                 "amount": "maybe less"
+#             },
+#             {
+#                 "date": "1 april",
+#                 "amount": "incredible quantity"
+#             }
+#         ]
+#         context = {
+#             "method": request.method,
+#             "year": request.POST.get("year"),
+#             "yearlydata": mockdata
+#         }
+#         return render(request, "reports/yearly.html", context)
+#     return render(request, "reports/yearly.html")
+
+def years(request):
+    html = "<html><body>This is the <em>years</em> page.</body></html>"
+    return HttpResponse(html)
+
+def months(request, year: int):
+    html = f"<html><body>This is the <em>months</em> page. The given year is {year}.</body></html>"
+    return HttpResponse(html)
+
+def days(request, year: int, month: int):
+    html = f"<html><body>This is the <em>days</em> page. The given year is {year}, month is {month}.</body></html>"
+    return HttpResponse(html)
+
+def report(request, year: int, month: int, day: int):
+    html = f"<html><body>This is the <em>report</em> page. The given year is {year}, month is {month}, day is {day}.</body></html>"
+    return HttpResponse(html)
