1ae25e6e7c7b0b0932a2f9a48a1b7227a5ff9ee3
T Bailey <tobyjonahbee@gmail.com>
2020-02-17 18:39:37 +0000

    Removed obsolete material/ reference.

--- server/reports/urls.py
+++ server/reports/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
-from .views import years, months, days, report, material
+from .views import years, months, days, report
 
 urlpatterns = [
     path("", years, name="report-list-years"),
     path("<int:year>/", months, name="report-list-months"),
     path("<int:year>/<int:month>/", days, name="report-list-days"),
     path("<int:year>/<int:month>/<int:day>/", report, name="report-get"),
-    path("material/", material, name="material-test")
 ]
