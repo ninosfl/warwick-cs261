b8bd42c3412420157894d0dd8b5b813be3acc1a8
T Bailey <tobyjonahbee@gmail.com>
2020-02-18 23:13:04 +0000

    Using multiple validation functions - more modular code!

--- server/reports/views.py
+++ server/reports/views.py
@@ -1,135 +1,131 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from calendar import Calendar
 import datetime
+from django.shortcuts import render
 
 def list_years(request):
+    """ List all years available. """
     placeholder_years = [y for y in range(2000, 2020 + 1)]
-    context = { "years": placeholder_years }
+    context = {"years": placeholder_years}
     return render(request, "reports/years.html", context)
 
 def list_months(request, year: int):
-    # Check for year validity
+    """ List all months available in the given year. """
+
     now = datetime.datetime.now()
-    if year > now.year:
-        error_message = f"The year {year} is in the future. There are no trades listed."
-        context = { "error_message": error_message }
+
+    result, err = is_year_valid(year, now)
+    if not result:
+        context = {"error_message": err}
         return render(request, "errors/errorpage.html", context)
-    elif year < 1970:
-        error_message = f"The year {year} is too far in the past. There are no trades listed."
-        context = { "error_message": error_message }
-        return render(request, "errors/errorpage.html", context)
-    else:
 
-        # Get way to transform int months into month strings
-        month_names = {
-            1: "January",
-            2: "February",
-            3: "March",
-            4: "April",
-            5: "May",
-            6: "June",
-            7: "July",
-            8: "August",
-            9: "September",
-            10: "October",
-            11: "November",
-            12: "December"
-        }
-
-        # Get valid lists of months (as integers)
-        if year == now.year:
-            months = [m for m in range(1, min(12, now.month) + 1)]
-        else:
-            months = [m for m in range(1, 12 + 1)]
-
-        context = {
-            "month_names": month_names,
-            "months": months,
-            "year": year
-        }
-        return render(request, "reports/months.html", context)
+    # Year is valid - so get list of months (as integers)
+    months = get_months(year, now)
+    context = {
+        "months": months,
+        "year": year
+    }
+    return render(request, "reports/months.html", context)
 
 def list_days(request, year: int, month: int):
-    # Check for year validity
+    """ List all days available in the given month. """
     now = datetime.datetime.now()
-    if year > now.year:
-        error_message = f"The year {year} is in the future. There are no trades listed."
-        context = { "error_message": error_message }
-        return render(request, "errors/errorpage.html", context)
-    elif year < 1970:
-        error_message = f"The year {year} is too far in the past. There are no trades listed."
-        context = { "error_message": error_message }
-        return render(request, "errors/errorpage.html", context)
-    else:
 
-        # Check for month validity
-        if month < 1 or month > 12:
-            error_message = f"Month values are from 1 to 12. The given month, {month}, is invalid."
-            context = { "error_message": error_message }
-            return render(request, "errors/errorpage.html", context)
-        elif year == now.year and month > now.month:
-            error_message = f"The month {month} is in the future. There are no trades listed."
-            context = { "error_message": error_message }
-            return render(request, "errors/errorpage.html", context)
-        else:
+    result, err = is_month_valid(year, now)
+    if not result:
+        context = {"error_message": err}
+        return render(request, "errors/errorpage.html", context)
 
-            # Get list of valid days for that month
-            c = Calendar()
-            if year == now.year and month == now.month:
-                days = [d for d in c.itermonthdays(year, month) if d != 0 and d <= now.day]
-            else:
-                days = [d for d in c.itermonthdays(year, month) if d != 0]
-            
-            context = {
-                "days": days,
-                "year": year,
-                "month": month
-            }
-            return render(request, "reports/days.html", context)
+    # Get list of valid days for that month
+    days = get_days(year, month, now)
+
+    context = {
+        "days": days,
+        "year": year,
+        "month": month
+    }
+    return render(request, "reports/days.html", context)
 
 def report(request, year: int, month: int, day: int):
-    # Check for year validity
-    now = datetime.datetime.now()
+    """ Fetch the report for the given day in the given month of the given year. """
+
+    result, err = is_day_valid(year, month, day)
+
+    if not result:
+        context = {"error_message": err}
+        return render(request, "errors/errorpage.html", context)
+
+    context = {
+        "year": year,
+        "month": month,
+        "day": day
+    }
+    return render(request, "reports/report.html", context)
+
+
+def is_year_valid(year: int, now=datetime.datetime.now()):
+    """ Checks if a given year is valid. """
+    # TODO: Make now use django timezone
+
     if year > now.year:
         error_message = f"The year {year} is in the future. There are no trades listed."
-        context = { "error_message": error_message }
-        return render(request, "errors/errorpage.html", context)
-    elif year < 1970:
+        return False, error_message
+
+    if year < 1970:
         error_message = f"The year {year} is too far in the past. There are no trades listed."
-        context = { "error_message": error_message }
-        return render(request, "errors/errorpage.html", context)
-    else:
+        return False, error_message
 
-        # Check for month validity
-        if month < 1 or month > 12:
-            error_message = f"Month values are from 1 to 12. The given month, {month}, is invalid."
-            context = { "error_message": error_message }
-            return render(request, "errors/errorpage.html", context)
-        elif year == now.year and month > now.month:
-            error_message = f"The month {month} is in the future. There are no trades listed."
-            context = { "error_message": error_message }
-            return render(request, "errors/errorpage.html", context)
-        else:
+    return True, None
 
-            # Get list of valid days for the month, making sure they don't go
-            # into the future.
-            c = Calendar()
-            if year == now.year and month == now.month:
-                days = [d for d in c.itermonthdays(year, month)
-                        if d != 0 and d <= now.day]
-            else:
-                days = [d for d in c.itermonthdays(year, month) if d != 0]
 
-            if day not in days:
-                error_message = f"There are no trades listed for {day}/{month}/{year}."
-                context = { "error_message": error_message }
-                return render(request, "errors/errorpage.html", context)
-            else:
+def is_month_valid(year: int, month: int, now=datetime.datetime.now()):
+    """ Checks if a given year and month pair are valid. """
+    # TODO: Make now use django timezone
 
-                context = {
-                    "year": year,
-                    "month": month,
-                    "day": day
-                }
-                return render(request, "reports/report.html", context)
+    result, err = is_year_valid(year, now)
+    if not result:
+        return result, err
+
+    if month < 1 or month > 12:
+        error_message = f"Month values are from 1 to 12. The given month, {month}, is invalid."
+        return False, error_message
+
+    if year == now.year and month > now.month:
+        error_message = f"The month {month} is in the future. There are no trades listed."
+        return False, error_message
+
+    return True, None
+
+
+def is_day_valid(year: int, month: int, day: int, now=datetime.datetime.now()):
+    """ Checks if a given year, month, and day are valid. """
+    # TODO: Make now use django timezone
+
+    result, err = is_month_valid(year, month, now)
+    if not result:
+        return result, err
+
+    if day not in get_days(year, month, now):
+        error_message = f"There are no trades listed for {day}/{month}/{year}."
+        return False, error_message
+
+    return True, None
+
+def get_months(year: int, now=datetime.datetime.now()):
+    """ Given a year, get all the valid months within it. """
+    # TODO: Make now use django timezone
+
+    if year == now.year:
+        return list(range(1, min(12, now.month) + 1))
+
+    return list(range(1, 12 + 1))
+
+def get_days(year: int, month: int,
+             now=datetime.datetime.now(), calendar=Calendar()):
+    """ Given a year and a month, get all the valid days of that month of that year. """
+    # TODO: Make now use django timezone
+
+    if year == now.year and month == now.month:
+        return [d for d in calendar.itermonthdays(year, month)
+                if d != 0 and d <= now.day]
+
+    return [d for d in calendar.itermonthdays(year, month) if d != 0]
