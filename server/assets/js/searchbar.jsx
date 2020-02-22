353579e61c75b9388687445f20cc82d4aa566c42
ischi09 <ishaan.shamanna@gmail.com>
2020-02-22 17:58:52 +0000

    Added scroll bar at the bottom when you scroll far enough down... it seems to have been positioned to the back to it is unresponsive

--- server/assets/js/searchbar.jsx
+++ server/assets/js/searchbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -6,11 +7,22 @@ import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
 import { fade, makeStyles } from '@material-ui/core/styles';
-import MenuIcon from '@material-ui/icons/Menu';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Fab from '@material-ui/core/Fab';
 import SearchIcon from '@material-ui/icons/Search';
 import HomeIcon from '@material-ui/icons/Home';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import Zoom from '@material-ui/core/Zoom';
 
 export{SearchBar};
+
+const topStyles = makeStyles(theme => ({
+  root: {
+    position: 'fixed',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+}));
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -65,6 +77,46 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+// This is the beginning of the scroll button, This is there incase the user wants to go back to the top
+function ScrollTop(props) {
+  const { children, window } = props;
+  const classes = topStyles();
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = event => {
+    const anchor = (event.target.ownerDocument || document).querySelector('#back-to-top-anchor');
+
+    if (anchor) {
+      anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }
+  };
+
+  return (
+    <Zoom in={trigger}>
+      <div onClick={handleClick} role="presentation" className={classes.root}>
+        {children}
+      </div>
+    </Zoom>
+  );
+}
+
+ScrollTop.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+//End of the scroll button
+
 // General React Component to add buttons to our bar
 function BarButton(props){
   return (<Button
@@ -83,7 +135,7 @@ function SearchBar() {
   return (
     <div className={classes.root}>
       <AppBar position="static">
-        <Toolbar> 
+        <Toolbar  id="back-to-top-anchor"> 
           {/* <IconButton
             edge="start"
             className={classes.menuButton}
@@ -133,6 +185,12 @@ function SearchBar() {
           </div>
         </Toolbar>
       </AppBar>
+      {/* This part does the scroll button with the arrow mark */}
+      <ScrollTop > 
+          <Fab color="primary" size="small" aria-label="scroll back to top">
+            <KeyboardArrowUpIcon />  
+          </Fab>
+      </ScrollTop>
     </div>
   );
 }
