d4d36f33995fa221f0b646530f7ceddf7c6549b0
DavidTrindade <d.ferreira@warwick.ac.uk>
2020-02-24 22:15:30 +0000

    Displays entries from a table in the db

--- server/reports/models.py
+++ server/reports/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Report(models.Model):
+	date_of_trade = models.DateField()
+	trade_ID = models.IntegerField()
+	product = models.CharField(max_length=50)
--- server/reports/views.py
+++ server/reports/views.py
@@ -2,6 +2,8 @@ from calendar import Calendar
 from django.utils import timezone
 from django.shortcuts import render
 
+from .models import Report
+
 def list_years(request):
     """ List all years available. """
     placeholder_years = [y for y in range(2000, 2020 + 1)]
@@ -49,15 +51,18 @@ def report(request, year: int, month: int, day: int):
     """ Fetch the report for the given day in the given month of the given year. """
 
     result, err = is_day_valid(year, month, day)
-
+	
     if not result:
         context = {"error_message": err}
         return render(request, "errors/errorpage.html", context)
 
+    reports = Report.objects.all()
+
     context = {
         "year": year,
         "month": month,
-        "day": day
+        "day": day,
+        "reports": reports
     }
     return render(request, "reports/report.html", context)
 
