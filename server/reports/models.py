cc39bbedbc6a46bd9656c76e3051ca4f5944800d
Valvify <Valvify@users.noreply.github.com>
2020-02-25 13:19:53 +0000

    added the rest of the things.

--- server/reports/models.py
+++ server/reports/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
 class Report(models.Model):
-	date_of_trade = models.DateField()
-	trade_id = models.IntegerField()
-	product = models.CharField(max_length=50)
-    buying_party = models.IntegerField() # Company ID
-    selling_party = models.IntegerField()
-    notional_amount = models.FloatField() # TODO: Specify to two decimal places?
-    quantity = models.Integer()
+    date_of_trade = models.DateField()
+    trade_id = models.IntegerField()
+    product = models.CharField(max_length=50)
+    buying_party = models.IntegerField() # A Company ID
+    selling_party = models.IntegerField() # A Company ID
+    notional_amount = models.DecimalField(max_digits=20, decimal_places=4)
+    quantity = models.IntegerField()
+    notional_currency = models.CharField(max_length=3)
+    maturity_date = models.DateField()
+    underlying_price = models.DecimalField(max_digits=20, decimal_places=4)
+    underlying_currency = models.CharField(max_length=3)
+    strike_price = models.DecimalField(max_digits=20, decimal_places=4)
