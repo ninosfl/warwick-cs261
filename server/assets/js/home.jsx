3f48463958c4de9ca099dc85a8f372064c7c2197
ischi09 <ishaan.shamanna@gmail.com>
2020-03-05 14:58:11 +0000

    Added in the buttons

--- server/assets/js/home.jsx
+++ server/assets/js/home.jsx
@@ -1,34 +1,43 @@
 import DateFnsUtils from '@date-io/date-fns';
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 import {KeyboardDatePicker, MuiPickersUtilsProvider} from '@material-ui/pickers';
 export {MaterialUIPickers}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(3, 1, 2),
+      alignItems: "centre",
+    },
+  },
+}));
+
+function BarButton(props){
+  return (<Button
+  variant="contained"
+  color="primary"
+  disableElevation={true}
+  href={props.href}
+>
+  {props.text}
+</Button>);
+}
 
 function MaterialUIPickers() {
   // The first commit of Material-UI
   const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
-
+  const classes = useStyles();
   const handleDateChange = date => {
     setSelectedDate(date);
   };
 
   return (
     
-    
+    <div className={classes.root}> 
     <Grid container justify="space-around">
-    {/* <KeyboardDatePicker
-        disableToolbar
-        variant="inline"
-        format="MM/dd/yyyy"
-        margin="normal"
-        id="date-picker-inline"
-        label="Date picker inline"
-        value={selectedDate}
-        onChange={handleDateChange}
-        KeyboardButtonProps={{
-        'aria-label': 'change date',
-        }}
-    /> */}
     <MuiPickersUtilsProvider utils={DateFnsUtils} >
         <KeyboardDatePicker
             margin="normal"
@@ -42,16 +51,17 @@ function MaterialUIPickers() {
             }}
         />
     </MuiPickersUtilsProvider>
-    {/* <KeyboardTimePicker
-        margin="normal"
-        id="time-picker"
-        label="Time picker"
-        value={selectedDate}
-        onChange={handleDateChange}
-        KeyboardButtonProps={{
-        'aria-label': 'change time',
-        }}
-    /> */}
     </Grid>
+    {/* Will need to modifiy these buttons to work with new URLs */}
+      <BarButton 
+        text="Trades"
+        href="/trades" 
+      />
+      <BarButton
+        text="Reports"
+        href="/trades"
+      />
+    </div>
+
   );
 }
