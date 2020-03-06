e7156c0cb8ca41ec98af549eca7d467c0564e2d6
Marinos Flangofas <marinos.flangofas@warwick.ac.uk>
2020-03-06 22:52:06 +0000

    Made listing of trades per day
    Basically copied the reports way of showing trades and added a button
    that leads to the correct trade editing page

--- server/trades/urls.py
+++ server/trades/urls.py
@@ -5,7 +5,7 @@ from .views import (enter, form, list_months, list_days, list_years,
 urlpatterns = [
     path("", enter, name='trades-new'),
     path("form/", form, name='enter-new-trade'),
-    path("edit/<str:trade_id>/", edit_trade, name='enter-new-trade'),
+    path("edit/<str:trade_id>/", edit_trade, name='trades-edit'),
     path("list/", list_years, name="trades-list-years"), 
     path("list/<int:year>/", list_months, name="trades-list-months"),
     path("list/<int:year>/<int:month>/", list_days, name="trades-list-days"),
--- server/trades/views.py
+++ server/trades/views.py
@@ -1,3 +1,6 @@
+from datetime import date
+from calendar import Calendar
+
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from trades.models import DerivativeTrade
@@ -38,8 +41,7 @@ def list_years(request):
     """ List all years available. """
     placeholder_years = [y for y in range(2020, 2000 - 1, -1)]
     context = {"years": placeholder_years}
-    return render(request, "reports/years.html", context)
-
+    return render(request, "trades/years.html", context)
 
 def list_months(request, year: int):
     """ List all months available in the given year. """
@@ -57,11 +59,7 @@ def list_months(request, year: int):
         "months": months,
         "year": year
     }
-    return render(request, "reports/months.html", context)
-
-
-def list_all_of_day(request):
-    pass # TODO
+    return render(request, "trades/months.html", context)
 
 def list_days(request, year: int, month: int):
     """ List all days available in the given month. """
@@ -80,4 +78,73 @@ def list_days(request, year: int, month: int):
         "year": year,
         "month": month
     }
-    return render(request, "reports/days.html", context)
+    return render(request, "trades/days.html", context)
+
+def list_all_of_day(request, year, month, day):
+    daily_trades = DerivativeTrade.objects.filter(date_of_trade__date=date(year, month, day))
+    return render(request, "trades/trades_list.html", {"trades": daily_trades, "day": day, "month": month, "year": year})
+
+def is_year_valid(year: int, now=timezone.now()):
+    """ Checks if a given year is valid. """
+
+    if year > now.year:
+        error_message = f"The year {year} is in the future. There are no trades listed."
+        return False, error_message
+
+    if year < 1970:
+        error_message = f"The year {year} is too far in the past. There are no trades listed."
+        return False, error_message
+
+    return True, None
+
+
+def is_month_valid(year: int, month: int, now=timezone.now()):
+    """ Checks if a given year and month pair are valid. """
+
+    result, err = is_year_valid(year, now)
+    if not result:
+        return result, err
+
+    if month < 1 or month > 12:
+        error_message = f"Month values are from 1 to 12. The given month, {month}, is invalid."
+        return False, error_message
+
+    if year == now.year and month > now.month:
+        error_message = f"The month {month} is in the future. There are no trades listed."
+        return False, error_message
+
+    return True, None
+
+
+def is_day_valid(year: int, month: int, day: int, now=timezone.now()):
+    """ Checks if a given year, month, and day are valid. """
+
+    result, err = is_month_valid(year, month, now)
+    if not result:
+        return result, err
+
+    if day not in get_days(year, month, now):
+        error_message = f"There are no trades listed for {day}/{month}/{year}."
+        return False, error_message
+
+    return True, None
+
+
+def get_months(year: int, now=timezone.now()):
+    """ Given a year, get all the valid months within it. """
+
+    if year == now.year:
+        return list(range(1, min(12, now.month) + 1))
+
+    return list(range(1, 12 + 1))
+
+
+def get_days(year: int, month: int,
+             now=timezone.now(), calendar=Calendar()):
+    """ Given a year and a month, get all the valid days of that month of that year. """
+
+    if year == now.year and month == now.month:
+        return [d for d in calendar.itermonthdays(year, month)
+                if d != 0 and d <= now.day]
+
+    return [d for d in calendar.itermonthdays(year, month) if d != 0]
