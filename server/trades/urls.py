f1414c1a18e17fd6cd7c1a694d96ef169ca7a003
T Bailey <tobyjonahbee@gmail.com>
2020-03-06 15:47:28 +0000

    Syntax fix.

--- server/trades/urls.py
+++ server/trades/urls.py
@@ -4,5 +4,5 @@ from .views import enter, form, edit_trade
 urlpatterns = [
     path("", enter, name='trades-new'),
     path("form/", form, name='enter-new-trade'),
-    path("edit/<int: id/", edit_trade, name='enter-new-trade')
+    path("edit/<int:id>/", edit_trade, name='enter-new-trade')
 ]
