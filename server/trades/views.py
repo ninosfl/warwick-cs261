7a75067989ed1afda3533b88b5af7947fc68ceac
Marinos Flangofas <marinos.flangofas@warwick.ac.uk>
2020-03-06 15:47:38 +0000

    Views for showing list of trades

--- server/trades/views.py
+++ server/trades/views.py
@@ -13,3 +13,48 @@ def form(request):
 
 def home(request):
     return render(request, "trades/home.html")
+
+def list_years(request):
+    """ List all years available. """
+    placeholder_years = [y for y in range(2020, 2000 - 1, -1)]
+    context = {"years": placeholder_years}
+    return render(request, "reports/years.html", context)
+
+
+def list_months(request, year: int):
+    """ List all months available in the given year. """
+
+    now = timezone.now()
+
+    result, err = is_year_valid(year, now)
+    if not result:
+        context = {"error_message": err}
+        return render(request, "errors/errorpage.html", context)
+
+    # Year is valid - so get list of months (as integers)
+    months = get_months(year, now)
+    context = {
+        "months": months,
+        "year": year
+    }
+    return render(request, "reports/months.html", context)
+
+
+def list_days(request, year: int, month: int):
+    """ List all days available in the given month. """
+    now = timezone.now()
+
+    result, err = is_month_valid(year, month, now)
+    if not result:
+        context = {"error_message": err}
+        return render(request, "errors/errorpage.html", context)
+
+    # Get list of valid days for that month
+    days = get_days(year, month, now)
+
+    context = {
+        "days": days,
+        "year": year,
+        "month": month
+    }
+    return render(request, "reports/days.html", context)
