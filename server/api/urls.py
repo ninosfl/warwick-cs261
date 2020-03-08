07e4fb3ab8ddf9e7ea508d86a8a11517ecd63975
T Bailey <tobyjonahbee@gmail.com>
2020-03-08 14:32:45 +0000

    Minor formatting.

--- server/api/urls.py
+++ server/api/urls.py
@@ -9,11 +9,11 @@ urlpatterns = [
     path("validate/product/", api_main, {"func": validate_product}, name="api-validate-product"),
     path("validate/trade/", api_main, {"func": ai_magic}, name="api-validate-trade"),
     path("validate/maturitydate/", api_main, {"func": validate_maturity_date}, name="api-validate-maturitydate"),
+    path("corrections/", api_main, {"func": correction}, name="api-correction"),
     path("submit/", api_main, {"func": submit_trade}, name="api-submit"),
     path("currencies/<date_str>/", currencies),
     path("currencies/", currencies),
     path("company/<company_name>/product/<product_name>/", company_product),
     path("company/<company_name>/", company),
-    path("corrections/", api_main, {"func": correction}, name="api-correction"),
     path("product/<product_name>/", product),
 ]
